d19d6b28db8268415464fde8446d77f27551906b
Takagi <iam.takagi.dev@gmail.com>
2021-04-04 01:51:40 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,5 @@
 import BotClient from './struct/BotClient'
 
-import express from 'express'
-const app: express.Express = express()
-
-app.get('/', async (req: any, res: any) => {
-    const client = req.query.client
-    if (client === 'gas') {
-        return res.send('Glitch woke up')
-    }
-})
-
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ', listener.address());
-});
-
 const client = new BotClient();
 
-client.start(process.env.TOKEN);
+client.start(process.env.BOT_TOKEN);
